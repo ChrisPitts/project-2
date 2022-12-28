aa56912c144d2fefa6ee54c3ac4a4c6da3cc2bde
Chris Pitts <cp25393@gmail.com>
2022-12-28 16:17:11 -0600

    finished basic HTML layout

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var api_key = "x3Ip9aCOKp7hWVkDKBWBSOvGeKE1MmrZ";
+
+// function makeAjaxRequest(){
+//     let limit = 5;
+//     let search = "Homer Simpson";
+//     $.ajax({
+//         url: `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${api_key}&limit=${limit}`,
+//         success: (result)=>{
+//             result.data.forEach(element => {
+//                 console.log(element.url);
+//             });
+//             console.log(result.data);
+//         }
+//     })
+// }
