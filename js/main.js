71fe10017b9db8f7464c60e007197801567090d1
Chris Pitts <cp25393@gmail.com>
2022-12-28 17:02:44 -0600

    finished basic search algorithm

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 var api_key = "x3Ip9aCOKp7hWVkDKBWBSOvGeKE1MmrZ";
 
-// function makeAjaxRequest(){
-//     let limit = 5;
-//     let search = "Homer Simpson";
-//     $.ajax({
-//         url: `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${api_key}&limit=${limit}`,
-//         success: (result)=>{
-//             result.data.forEach(element => {
-//                 console.log(element.url);
-//             });
-//             console.log(result.data);
-//         }
-//     })
-// }
+function makeAjaxRequest(){
+    let limit = 10;
+    let search = "cheeseburger";
+    $.ajax({
+        url: `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${api_key}&limit=${limit}`,
+        success: (result)=>{
+            result.data.forEach(element => {
+                console.log(element);
+                let image = document.createElement("img");
+                image.src = element.images.original.url;
+                $("#photo-container").append(image);
+            });
+            console.log(result.data);
+        }
+    })
+}
+
+makeAjaxRequest();
