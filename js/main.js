07a04bddc48143f11a230817f0438d1df85183b0
Chris Pitts <cp25393@gmail.com>
2022-12-29 09:02:36 -0600

    added some comments

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
 var api_key = "x3Ip9aCOKp7hWVkDKBWBSOvGeKE1MmrZ";
 
 function searchButtonClicked(){
-    $("#photo-container").html("");
-    let limit = 20;
+    $("#photo-container").html(""); // clear the images in the container
+
+    let limit = 20; // limit for how many images will show on the page 
     let search = $("#search-input").val();
+
     $.ajax({
         url: `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${api_key}&limit=${limit}`,
+
         success: (result)=>{
             result.data.forEach(element => {
-                // console.log(element);
+                // Create the img element and its container
                 let imageContainer = document.createElement("div");
                 let image = document.createElement("img");
                 image.src = element.images.original.url;
+                
+                // Append the image and its container to the DOM
                 imageContainer.appendChild(image)
                 $("#photo-container").append(imageContainer);
-            });
-            // console.log(result.data);
-        }
-    })
+            }/* end forEach */);
+        } //end success
+    }) // end ajax
 }
